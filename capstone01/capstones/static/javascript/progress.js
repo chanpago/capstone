ee24fd130d4c8800b08f54b436b7783397c773d6
KangYelim <kyl9164@naver.com>
2022-05-25 16:44:53 +0900

    js 기능별로 분리

---
+++ capstone01/capstones/static/javascript/progress.js
@@ -0,0 +1,28 @@
+//progress bar 부분
+    var progressBar = $('.progress-bar');
+    var progressNumber=0;
+
+    var intervalId= setInterval(function(){  //progress bar 동작
+   
+            progressNumber+=2;
+            progressBar.css('width',progressNumber+'%');
+            progressBar.attr('aria-valuenow',progressNumber);
+            //console.log(progressNumber);
+
+            if(progressNumber=="100"){
+                clearInterval(intervalId);
+                document.getElementById('icon2').innerHTML='<i class="bi2 bi-check-circle"></i>';
+            }
+    },100);
+
+
+
+    // const a =  document.querySelector('.progress-bar');
+    // a.addEventListener(showIcon());
+
+    // function showIcon() {
+    //     // document.getElementById('icon2').innerHTML='<i class="bi2 bi-check-circle"></i>';
+    //     if(a.width==514){
+    //         document.getElementById('icon2').innerHTML='<i class="bi2 bi-check-circle"></i>';
+    //     }
+    // }
